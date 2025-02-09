c10cdcfa9b58a6b8c746949d915a9fd66582f929
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2025-02-09 00:01:53 -0300

    feat: creating the relationship method between Task and User

--- todo/domain/src/main/java/com/domain/tasks/entities/Task.java
+++ todo/domain/src/main/java/com/domain/tasks/entities/Task.java
@@ -1,41 +1,36 @@
 package com.domain.tasks.entities;
 
+import com.domain.identity.entities.User;
 import com.domain.shared.entities.Entity;
 import com.domain.shared.utils.ValidationResult;
 import com.domain.shared.utils.ValidatorUtil;
-import jakarta.validation.ConstraintViolation;
-import jakarta.validation.Validation;
-import jakarta.validation.Validator;
-import jakarta.validation.ValidatorFactory;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDateTime;
-import java.util.List;
-import java.util.Collections;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class Task extends Entity {
     @NotBlank(message = "O título não pode estar vazio")
     @Size(min = 3, max = 100, message = "O título deve ter entre 3 e 100 caracteres")
     private String title;
-
     @Size(max = 500, message = "O título deve ter no máximo 500 caracteres")
     private String description;
     private LocalDateTime createdDate;
     private LocalDateTime updatedDate;
     private boolean isComplete;
+    private User user;
 
     public Task(
             String title,
-            String description) {
+            String description,
+            User user) {
         super();
         this.title = title;
         this.description = description;
         createdDate = LocalDateTime.now();
         updatedDate = createdDate;
         isComplete = false;
+        this.user = user;
     }
 
     public ValidationResult validate() {
