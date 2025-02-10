214cd1cef9908738d718b5da57d42ac74b7c60d1
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2025-02-10 15:52:40 -0300

    feat: development of the getById() method

--- todo/domain/src/main/java/com/domain/tasks/repositories/ITaskRepository.java
+++ todo/domain/src/main/java/com/domain/tasks/repositories/ITaskRepository.java
@@ -2,6 +2,9 @@ package com.domain.tasks.repositories;
 
 import com.domain.tasks.entities.Task;
 
+import java.util.UUID;
+
 public interface ITaskRepository {
     public void save(Task task);
+    public Task getById(UUID id);
 }
