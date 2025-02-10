dedabc9565b2a23666cbc9c26576ba16efe6d13a
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2025-02-10 10:44:22 -0300

    refactor: making the class abstract

--- todo/domain/src/main/java/com/domain/shared/valueobjetcts/ValueObject.java
+++ todo/domain/src/main/java/com/domain/shared/valueobjetcts/ValueObject.java
@@ -3,7 +3,7 @@ package com.domain.shared.valueobjetcts;
 import com.domain.shared.utils.ValidationResult;
 import com.domain.shared.utils.ValidatorUtil;
 
-public class ValueObject {
+public abstract class ValueObject {
     public ValidationResult validate() {
         return ValidatorUtil.validate(this);
     }
