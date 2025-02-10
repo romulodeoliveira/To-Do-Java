44ba7a1383c89813606d9d7916bf53d213b8733a
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2025-02-10 10:59:28 -0300

    feat: adding the validations

--- todo/domain/src/main/java/com/domain/identity/valueobjects/Name.java
+++ todo/domain/src/main/java/com/domain/identity/valueobjects/Name.java
@@ -1,7 +1,12 @@
 package com.domain.identity.valueobjects;
 
-public class Name {
+import com.domain.shared.valueobjetcts.ValueObject;
+import jakarta.validation.constraints.NotBlank;
+
+public class Name extends ValueObject {
+    @NotBlank(message = "O nome não pode estar vazio")
     private String firstName;
+    @NotBlank(message = "O sobrenome não pode estar vazio")
     private String lastName;
 
     public Name(
@@ -11,6 +16,7 @@ public class Name {
         this.lastName = lastName;
     }
 
+    // functions
     public String toString() {
         return firstName + " " + lastName;
     }
