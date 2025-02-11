c5e30f1f7182eb45ba7e3e2a003b637f767d782c
Romulo de Oliveira <devromulodeoliveira@gmail.com>
2025-02-11 19:24:36 -0300

    feat: initial development of controllers

--- todo/domain/src/main/java/com/domain/shared/entities/BaseEntity.java
+++ todo/domain/src/main/java/com/domain/shared/entities/BaseEntity.java
@@ -2,10 +2,17 @@ package com.domain.shared.entities;
 
 import com.domain.shared.utils.ValidationResult;
 import com.domain.shared.utils.ValidatorUtil;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
 
 import java.util.UUID;
 
+@MappedSuperclass
 public abstract class Entity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     protected UUID id;
 
     protected Entity() {
